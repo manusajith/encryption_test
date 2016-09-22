a1f99e181b6457825874bc50076253a85948faae
Manu S Ajith <neo@codingarena.in>
2016-09-22 10:42:08 +0530

    Added doctests for encrypt/2 function

--- lib/encryption.ex
+++ lib/encryption.ex
@@ -19,6 +19,24 @@ defmodule Encryption do
   end
 
 
+  @doc """
+  Mostly for internal use but can be used, if key is known
+
+  iex> Encryption.encrypt(nil, "1")
+  "Key should be present"
+
+  iex> Encryption.encrypt("2", "1")
+  "12"
+
+  iex> Encryption.encrypt("2", 1)
+  "Key and Data should be binary and present"
+
+  iex> Encryption.encrypt(2, "1")
+  "Key and Data should be binary and present"
+
+  iex> Encryption.encrypt(2, 1)
+  "Key and Data should be binary and present"
+  """
   def encrypt(key, data) when is_nil(key), do: "Key should be present"
   def encrypt(key, data) when is_binary(key) and is_binary(data) do
     Process.sleep(2000)
